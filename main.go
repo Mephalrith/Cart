2e26907f5452db31226c181ea3377998cc8ad4db
mephalrith <xsadrithx@yahoo.com>
2018-10-25 14:48:59 -0700

    Made corrections to options check

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"math"
+	"os"
 	"strings"
 )
 
@@ -31,53 +33,42 @@ func readInput(input string, jsonType interface{}) {
 }
 
 func checkOptions(item Item, price Price) bool {
-	/* TODO:
-	 * i'm having trouble figuring out how to
-	 * differentiate between the options.... things
-	 * are evaluating to true when they shouldn't be
-	 */
-	isEqual := false
-	for key, val := range item.Options {
-		if j, ok := price.Options[key]; ok {
-			for _, v := range j {
+	counter := 0
+
+	for key, vals := range price.Options {
+		if val, ok := item.Options[key]; ok {
+			for _, v := range vals {
 				if v == val {
-					isEqual = true
+					counter++
 				}
 			}
 		}
 	}
-	return isEqual
+
+	if counter == len(price.Options) {
+		return true
+	}
+
+	return false
 }
 
 func calculatePrice(item Item, price Price) int {
-	// TODO: markup is evaluating to 0, need to google more
-	markup := float64(price.BasePrice * (item.ArtistMarkup / 100))
-	fmt.Println(markup)
-
-	total := (price.BasePrice + int(Round(markup))) * item.Quantity
-	fmt.Println(total)
-
-	return total
-
-	// one.json should be:
-	//((4368 * 0.3) + 4368) * 2
-	//(1310.4 + 4368) * 2
-	//5678.4 * 2
-	//11356.8
+	markup := float64(price.BasePrice) * (float64(item.ArtistMarkup) / 100)
+	return (price.BasePrice + int(Round(markup))) * item.Quantity
 }
 
 func main() {
-	//reader := bufio.NewReader(os.Stdin)
+	reader := bufio.NewReader(os.Stdin)
 
-	//fmt.Println("Enter Cart path.json:")
-	//cartInput, _ := reader.ReadString('\n')
-	//readInput(cartInput, &cart)
-	readInput("one.json", &cart)
+	fmt.Println("Enter Cart path.json:")
+	cartInput, _ := reader.ReadString('\n')
+	readInput(cartInput, &cart)
+	//readInput("one.json", &cart)
 
-	//fmt.Println("Enter Base Prices path.json:")
-	//priceInput, _ := reader.ReadString('\n')
-	//readInput(priceInput, &basePrices)
-	readInput("prices.json", &basePrices)
+	fmt.Println("Enter Base Prices path.json:")
+	priceInput, _ := reader.ReadString('\n')
+	readInput(priceInput, &basePrices)
+	//readInput("prices.json", &basePrices)
 
 	cartTotal := 0
 
